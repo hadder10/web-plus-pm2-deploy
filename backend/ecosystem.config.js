cc79d5dca521b142658622c31a805abc73c5401b
hadder10 <casilyas1@gmail.com>
2026-04-14 04:08:31 +0300

    ya zaebalsa 20

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -2,8 +2,14 @@ const dotenv = require("dotenv");
 
 dotenv.config({ path: "./.env.deploy" });
 
-const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_REF, DEPLOY_REPOSITORY, DEPLOY_PATH } =
-  process.env;
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_REF,
+  DEPLOY_REPOSITORY,
+  DEPLOY_PATH,
+  DEPLOY_KEY,
+} = process.env;
 
 module.exports = {
   apps: [
@@ -19,6 +25,8 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
+      key: DEPLOY_KEY,
+      ssh_options: "ForwardAgent=yes",
       "pre-deploy-local": `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
       "post-deploy":
         "cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production",
--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -2,8 +2,14 @@ const dotenv = require("dotenv");
 
 dotenv.config({ path: "./.env.deploy" });
 
-const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_REF, DEPLOY_REPOSITORY, DEPLOY_PATH } =
-  process.env;
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_REF,
+  DEPLOY_REPOSITORY,
+  DEPLOY_PATH,
+  DEPLOY_KEY,
+} = process.env;
 
 module.exports = {
   deploy: {
@@ -13,6 +19,8 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
+      key: DEPLOY_KEY,
+      ssh_options: "ForwardAgent=yes",
       "post-deploy": "cd frontend && pwd && npm ci && npm run build",
     },
   },

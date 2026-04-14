eaca4f83ca709fa3657a4ba3ba034aa3984fff42
hadder10 <casilyas1@gmail.com>
2026-04-14 04:47:47 +0300

    ya zaebalsa 29

--- backend/scripts/deployEnv.sh
+++ backend/scripts/deployEnv.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 SSH_CONFIG="${1}"
 PROJECT_PATH="${2}"
-scp -Cr .env.deploy "$SSH_CONFIG:${PROJECT_PATH}/current/backend"
+SSH_KEY="${3:-/home/hadder/Загрузки/Бэкап_за_2026-03-08/some_ssh}"
+scp -i "$SSH_KEY" -C .env "$SSH_CONFIG:${PROJECT_PATH}/current/backend"
